5e8c4a539da2198cec6b41447eadd1fde75db75f
Rizki Ramadhani <89191457+rizkiramadhani624@users.noreply.github.com>
2021-10-19 00:16:31 +0700

    Tutorial dasar: Melihat databases, membuat, menggunakan, dan menghapus.

---
+++ membuat dan menghapus data.sql
@@ -0,0 +1,7 @@
+mysql> CREATE DATABASE mahasiswa; //query membuat data baru
+
+mysql> SHOW DATABASES; //query melihat daftar databases
+
+mysql> USE mahasiswa; //menggunakan data tertentu
+
+mysql> DROP DATABASE mahasiswa; // menghapus data tertentu
